f00d1babd2f1247e0f7a03d7e2fcd0ddbdab5a29
213311012 <numangunduz00@gmail.com>
2024-09-26 14:17:49 +0300

    Add project files.

---
+++ App.Repositories/Product.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace App.Repositories
+{
+    public class Product
+    {
+        public int? Id { get; set; }
+        public string?  Name { get; set; }
+        public decimal  Price { get; set; }
+        public int Stoct { get; set; }
+    }
+}
